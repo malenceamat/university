8fd9b81b026552b3d20d2673fed0cacdf7cb4a6b
malenceamat <yatikivan@gmail.com>
2024-01-18 18:26:45 +0300

    правки

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,20 +4,20 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
-    <title>SignIn Boxed | CORK - Multipurpose Bootstrap Dashboard Template </title>
-    <link rel="icon" type="image/x-icon" href="../src/assets/img/favicon.ico"/>
-    <link href="../layouts/vertical-dark-menu/css/light/loader.css" rel="stylesheet" type="text/css" />
-    <link href="../layouts/vertical-dark-menu/css/dark/loader.css" rel="stylesheet" type="text/css" />
-    <script src="../layouts/vertical-dark-menu/loader.js"></script>
+    <title>Login</title>
+    <link rel="icon" type="image/x-icon" href={{asset("../src/assets/img/favicon.ico")}}/>
+    <link href={{asset("../layouts/vertical-dark-menu/css/light/loader.css")}} rel="stylesheet" type="text/css" />
+    <link href={{asset("../layouts/vertical-dark-menu/css/dark/loader.css")}} rel="stylesheet" type="text/css" />
+    <script src={{asset("../layouts/vertical-dark-menu/loader.js")}}></script>
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
-    <link href="../src/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href={{asset("../src/bootstrap/css/bootstrap.min.css")}} rel="stylesheet" type="text/css" />
 
-    <link href="../layouts/vertical-dark-menu/css/light/plugins.css" rel="stylesheet" type="text/css" />
-    <link href="../src/assets/css/light/authentication/auth-boxed.css" rel="stylesheet" type="text/css" />
+    <link href={{asset("../layouts/vertical-dark-menu/css/light/plugins.css")}} rel="stylesheet" type="text/css" />
+    <link href={{asset("../src/assets/css/light/authentication/auth-boxed.css")}} rel="stylesheet" type="text/css" />
 
-    <link href="../layouts/vertical-dark-menu/css/dark/plugins.css" rel="stylesheet" type="text/css" />
-    <link href="../src/assets/css/dark/authentication/auth-boxed.css" rel="stylesheet" type="text/css" />
+    <link href={{asset("../layouts/vertical-dark-menu/css/dark/plugins.css")}} rel="stylesheet" type="text/css" />
+    <link href={{asset("../src/assets/css/dark/authentication/auth-boxed.css")}} rel="stylesheet" type="text/css" />
     <!-- END GLOBAL MANDATORY STYLES -->
 
 </head>
@@ -70,18 +70,11 @@
                                         </div>
 
                                         <div class="flex items-center justify-end mt-4">
-                                            <x-button class="ml-3">
+                                            <x-button class="ml-3" style="color: black!important;">
                                                 {{ __('Log in') }}
                                             </x-button>
                                         </div>
                                     </form>
-
-
-
-
-
-
-
 
                         </div>
 
