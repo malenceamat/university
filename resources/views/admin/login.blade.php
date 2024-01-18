579aa20726070f6d281baf555dc6f250fca3167b
malenceamat <yatikivan@gmail.com>
2024-01-18 18:16:57 +0300

    правки

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -5,28 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
     <title>SignIn Boxed | CORK - Multipurpose Bootstrap Dashboard Template </title>
-    <link rel="icon" type="image/x-icon" href="../src/assets/img/favicon.ico"/>
-    <link href="../layouts/vertical-dark-menu/css/light/loader.css" rel="stylesheet" type="text/css" />
-    <link href="../layouts/vertical-dark-menu/css/dark/loader.css" rel="stylesheet" type="text/css" />
-    <script src="../layouts/vertical-dark-menu/loader.js"></script>
+    <link rel="icon" type="image/x-icon" href={{asset("../src/assets/img/favicon.ico")}}/>
+    <link href={{asset("../layouts/vertical-dark-menu/css/light/loader.css")}} rel="stylesheet" type="text/css"/>
+    <link href={{asset("../layouts/vertical-dark-menu/css/dark/loader.css")}} rel="stylesheet" type="text/css"/>
+    <script src={{asset("../layouts/vertical-dark-menu/loader.js")}}></script>
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
-    <link href="../src/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href={{asset("../src/bootstrap/css/bootstrap.min.css")}} rel="stylesheet" type="text/css"/>
 
-    <link href="../layouts/vertical-dark-menu/css/light/plugins.css" rel="stylesheet" type="text/css" />
-    <link href="../src/assets/css/light/authentication/auth-boxed.css" rel="stylesheet" type="text/css" />
+    <link href={{asset("../layouts/vertical-dark-menu/css/light/plugins.css")}} rel="stylesheet" type="text/css"/>
+    <link href={{asset("../src/assets/css/light/authentication/auth-boxed.css")}} rel="stylesheet" type="text/css"/>
 
-    <link href="../layouts/vertical-dark-menu/css/dark/plugins.css" rel="stylesheet" type="text/css" />
-    <link href="../src/assets/css/dark/authentication/auth-boxed.css" rel="stylesheet" type="text/css" />
+    <link href={{asset("../layouts/vertical-dark-menu/css/dark/plugins.css")}} rel="stylesheet" type="text/css"/>
+    <link href={{asset("../src/assets/css/dark/authentication/auth-boxed.css")}} rel="stylesheet" type="text/css"/>
     <!-- END GLOBAL MANDATORY STYLES -->
 
 </head>
 <body class="form">
 
 <!-- BEGIN LOADER -->
-<div id="load_screen"> <div class="loader"> <div class="loader-content">
+<div id="load_screen">
+    <div class="loader">
+        <div class="loader-content">
             <div class="spinner-grow align-self-center"></div>
-        </div></div></div>
+        </div>
+    </div>
+</div>
 <!--  END LOADER -->
 
 <div class="auth-container d-flex">
@@ -60,126 +64,49 @@
                             </div>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
                             <x-guest-layout>
                                 <x-auth-card>
                                     <x-slot name="logo">
                                         <a href="/">
-                                            <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                                            <x-application-logo class="w-20 h-20 fill-current text-gray-500"/>
                                         </a>
                                     </x-slot>
 
                                     <!-- Session Status -->
-                                    <x-auth-session-status class="mb-4" :status="session('status')" />
+                                    <x-auth-session-status class="mb-4" :status="session('status')"/>
 
                                     <!-- Validation Errors -->
-                                    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                                    <x-auth-validation-errors class="mb-4" :errors="$errors"/>
 
                                     <form method="POST" action="{{ route('login') }}">
                                         @csrf
 
                                         <!-- Email Address -->
                                         <div>
-                                            <x-label for="email" :value="__('Email')" />
+                                            <x-label for="email" :value="__('Email')"/>
 
-                                            <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                                            <x-input id="email" class="block mt-1 w-full" type="email" name="email"
+                                                     :value="old('email')" required autofocus/>
                                         </div>
 
                                         <!-- Password -->
                                         <div class="mt-4">
-                                            <x-label for="password" :value="__('Password')" />
+                                            <x-label for="password" :value="__('Password')"/>
 
                                             <x-input id="password" class="block mt-1 w-full"
                                                      type="password"
                                                      name="password"
-                                                     required autocomplete="current-password" />
+                                                     required autocomplete="current-password"/>
                                         </div>
-
-                                        <!-- Remember Me -->
-                                        {{-- <div class="block mt-4">
-                                             <label for="remember_me" class="inline-flex items-center">
-                                                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                                                 <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                                             </label>
-                                         </div>--}}
-
                                         <div class="flex items-center justify-end mt-4">
-                                            {{-- @if (Route::has('password.request'))
-                                                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                                                     {{ __('Forgot your password?') }}
-                                                 </a>
-                                             @endif--}}
-
-                                            <x-button class="ml-3">
+                                            <x-button class="ml-3" style="color: black!important;">
                                                 {{ __('Log in') }}
                                             </x-button>
                                         </div>
                                     </form>
                                 </x-auth-card>
                             </x-guest-layout>
-
-
-
-
-
-
-
                         </div>
-
                     </div>
                 </div>
             </div>
@@ -191,7 +118,7 @@
 </div>
 
 <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
-<script src="../src/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src={{asset("../src/bootstrap/js/bootstrap.bundle.min.js")}}></script>
 <!-- END GLOBAL MANDATORY SCRIPTS -->
 
 
