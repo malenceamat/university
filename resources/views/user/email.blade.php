39fd808dc27b8904032624bcbce2b8e570710ffd
malenceamat <yatikivan@gmail.com>
2024-01-19 18:27:41 +0300

    правки

--- resources/views/user/email.blade.php
+++ resources/views/user/email.blade.php
@@ -3,11 +3,11 @@
     <div class="">
         <div class="card-body p-md-5 p-4 bg-size-cover" style="background-image: url({{asset('/storage/'. $data['image'])}});">
             <div class="py-md-5 py-4 text-center">
-                <a href="{{$data['button']}}" type="button" class="btn btn-email btn-lg fs-6 rounded mb-4" target="_blank" rel="noopener">
+                <a href="{{$data['button']}}" type="button" class="btn btn-email btn-lg fs-6 rounded mb-4" target="_blank" rel="noopener" style="background-color: #85d5e1!important;border-color: #85d5e1!important;">
                     &nbsp;{{$data['buttontext']}}
                 </a>
-                <p class="fw-normal text-light mt-4 mb-1" style="color: black!important;">{{$data['text']}}</p>
-                <a href="mailto:{{$data['email']}}" class="text-light" style="color: black!important;">{{$data['email']}}</a>
+                <p class="fw-normal text-light mt-4 mb-1" style="color: white!important;">{{$data['text']}}</p>
+                <a href="mailto:{{$data['email']}}" class="text-light" style="color: white!important;">{{$data['email']}}</a>
             </div>
         </div>
     </div>
