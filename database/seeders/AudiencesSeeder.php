e88d8bf3a92400c09ac1ddcde83fea86315c779b
malenceamat <yatikivan@gmail.com>
2024-02-01 16:32:12 +0300

    правки9

--- database/seeders/AudiencesSeeder.php
+++ database/seeders/AudiencesSeeder.php
@@ -19,15 +19,15 @@ class AudiencesSeeder extends Seeder
         //Audience::factory(3)->create();
 
         Audience::create([
-           'image' => 'content/2-4.svg',
+           'image' => 'content/new-2-4.svg',
             'head' => 'Курсы бакалавриата'
         ]);
         Audience::create([
-            'image' => 'content/3-5.svg',
+            'image' => 'content/new-3-5.svg',
             'head' => 'Курсы специалитета'
         ]);
         Audience::create([
-            'image' => 'content/1-2.svg',
+            'image' => 'content/new-1-2.svg',
             'head' => 'Курс магистратуры'
         ]);
     }
